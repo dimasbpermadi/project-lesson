abb595f7e9e8318581a3afdc90638a56d6b7e660
Dimas Bagus Permadi <dimasbagus.permadi@gmail.com>
2017-09-01 17:27:07 -0700

    Inisialisasi pertama untuk project belajar buat web corporate h2h

---
+++ corporate-h2h/src/main/java/co/id/payment/corporate/corporateh2h/CorporateH2hApplication.java
@@ -0,0 +1,12 @@
+package co.id.payment.corporate.corporateh2h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateH2hApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateH2hApplication.class, args);
+	}
+}
---
+++ corporate-h2h/src/test/java/co/id/payment/corporate/corporateh2h/CorporateH2hApplicationTests.java
@@ -0,0 +1,16 @@
+package co.id.payment.corporate.corporateh2h;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorporateH2hApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
