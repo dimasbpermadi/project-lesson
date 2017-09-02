ded892e15d133eda9454ff4ab4ed979251e1c793
Dimas Bagus Permadi <dimasbagus.permadi@gmail.com>
2017-09-01 22:28:25 -0700

    Configure spring data JPA

---
+++ corporate-h2h/src/main/resources/db/migration/V201709012059__Data_Init.sql
@@ -0,0 +1,56 @@
+insert into client (id, clientid, clientname, description)
+values ('aaa','PTMN0001','PT. Pertamina Persero, Tbk','PT. Pertamina Persero, Tbk Payments');
+
+insert into client (id, clientid, clientname, description)
+values ('bbb','PGN0001','PT. Perusahan Gas Negara, Tbk','PT. Perusahaan Gas Negara, Tbk Payments');
+
+insert into client (id, clientid, clientname, description)
+values ('ccc','GRDA0001','PT. Garuda Indonesia, Tbk','PT. Garuda Indonesia, Tbk Payments');
+
+insert into client (id, clientid, clientname, description)
+values ('ddd','ANTM0001','PT. Antam Persero, Tbk','PT. Antam Persero, Tbk Payments');
+
+insert into client (id, clientid, clientname, description)
+values ('eee','UNLV0001','PT. Unilever Persero, Tbk','PT. Unilever Persero, Tbk Payments');
+
+insert into client (id, clientid, clientname, description)
+values ('fff','INDST0001','PT. Indosat Ooreedoo Persero, Tbk','PT. Indosat Ooreedoo, Tbk Payments');
+
+insert into client (id, clientid, clientname, description)
+values ('ggg','GDGRM0001','PT. Gudang Garam, Tbk','PT. Gudang Garam, Tbk Payments');
+
+insert into clientpic (idpic, clientid, picname, picunit, picemail, piccontact)
+values
+('aaa','PTMN0001','Junee','Finance','junee@random.co.id','081234567890');
+
+insert into clientpic (idpic, clientid, picname, picunit, picemail, piccontact)
+values
+('bbb','PGN0001','Junee','Finance','junee@random.co.id','081234567890');
+
+insert into clientpic (idpic, clientid, picname, picunit, picemail, piccontact)
+values
+('ccc','GRDA0001','Anone','Finance','Anone@random.co.id','081234567890');
+
+insert into clientpic (idpic, clientid, picname, picunit, picemail, piccontact)
+values
+('ddd','ANTM0001','Anone','Finance','Anone@random.co.id','081234567890');
+
+insert into clientpic (idpic, clientid, picname, picunit, picemail, piccontact)
+values
+('eee','UNLV0001','Junee','Finance','junee@random.co.id','081234567890');
+
+insert into clientpic (idpic, clientid, picname, picunit, picemail, piccontact)
+values
+('fff','INDST0001','natte','Finance','natte@random.co.id','081234567890');
+
+insert into clientpic (idpic, clientid, picname, picunit, picemail, piccontact)
+values
+('ggg','GDGRM0001','Anone','Finance','Anone@random.co.id','081234567890');
+
+insert into clientips (ipid, clientid, clientip) values ('aaa','PTMN0001','10.10.10.01');
+insert into clientips (ipid, clientid, clientip) values ('bbb','PGN0001','10.10.10.02');
+insert into clientips (ipid, clientid, clientip) values ('ccc','GRDA0001','10.10.10.03');
+insert into clientips (ipid, clientid, clientip) values ('ddd','ANTM0001','10.10.10.04');
+insert into clientips (ipid, clientid, clientip) values ('eee','UNLV0001','10.10.10.05');
+insert into clientips (ipid, clientid, clientip) values ('fff','INDST0001','10.10.10.06');
+insert into clientips (ipid, clientid, clientip) values ('ggg','GDGRM0001','10.10.10.07');
