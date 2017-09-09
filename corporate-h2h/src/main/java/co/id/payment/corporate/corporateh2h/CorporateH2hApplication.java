361590e3bbad6596d167564b6a614c5367b72f16
Dimas Bagus Permadi <dimasbagus.permadi@gmail.com>
2017-09-09 15:14:44 -0700

    Pagination Implementation

--- corporate-h2h/src/main/java/co/id/payment/corporate/corporateh2h/CorporateH2hApplication.java
+++ corporate-h2h/src/main/java/co/id/payment/corporate/corporateh2h/CorporateH2hApplication.java
@@ -2,6 +2,8 @@ package co.id.payment.corporate.corporateh2h;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class CorporateH2hApplication {
@@ -9,4 +11,10 @@ public class CorporateH2hApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CorporateH2hApplication.class, args);
 	}
+	
+	@Bean
+	public SpringDataDialect springDataDialect() {
+		return new SpringDataDialect();	
+	}
+	
 }
