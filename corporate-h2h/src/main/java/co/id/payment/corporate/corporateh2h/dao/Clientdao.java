ec1952078baf569c65c38c13c74b84218f24205f
Dimas Bagus Permadi <dimasbagus.permadi@gmail.com>
2017-09-25 14:40:24 +0700

    clean repository

--- corporate-h2h/src/main/java/co/id/payment/corporate/corporateh2h/dao/Clientdao.java
+++
@@ -1,9 +0,0 @@
-package co.id.payment.corporate.corporateh2h.dao;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import co.id.payment.corporate.corporateh2h.entity.Client;
-
-public interface Clientdao extends PagingAndSortingRepository<Client, String> {
-
-}
